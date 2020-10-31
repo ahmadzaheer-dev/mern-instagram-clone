e4e5f97cfc52ec45a04a68df1a37746a935474b1
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-10-31 05:15:03 -0700

    Used Post Router

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ require('./db/mongooseConn');
 
 const userRouter = require('./routes/user');
 const profileRouter = require('./routes/profile');
+const postRouter = require('./routes/post')
 
 
 app.use(express.json());
 app.use(userRouter);
 app.use(profileRouter);
+app.use(postRouter);
+
 
 // PORT
 const port = process.env.PORT || 3000;
