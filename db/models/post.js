9563b52864c4de3bd65ca3349c4bfadffdd607a4
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-10-31 01:08:25 -0700

    Implemented: Schema for Post model

---
+++ db/models/post.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+    image: {
+        type: String,
+        required: true
+    },
+
+    caption: {
+        type: String,
+        required: true,
+    },
+
+    likes: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+                required: true
+            }
+        }
+    ],
+
+    comments: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+                required: true
+            },
+            caption: {
+                type: String,
+                required: true
+            }
+        }
+    ]
+})
+
+const post = mongoose.model('Post', postSchema);
+module.exports = post;
