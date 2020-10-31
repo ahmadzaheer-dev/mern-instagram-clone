f4a41bfe18c5f6b90983622a4bbe60f1fe584beb
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-10-30 23:34:57 -0700

    Implemented Authentication middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const User = require('../db/models/user');
+const jwt = require('jsonwebtoken');
+
+//AUTHENTICATION MIDDLEWARE
+const auth = async (req, res, next) => {
+    try{
+        const token = req.header('authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, 'thisisuserverification');
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+        if(!user) {
+            throw new Error('Please Authenticate');
+        }
+        req.user = user;
+        req.token = token;
+        next();
+    } catch(e){
+        res.status(400).send({ err: 'Please Authenticate' });
+    }
+}
+
+module.exports = auth;
