32bda968405be3a1c1387bb08e9746d89aabe2ff
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-11-02 12:05:33 -0800

    Implemented route for Unfollow a profile

--- routes/profile.js
+++ routes/profile.js
@@ -56,7 +56,6 @@ router.put('/api/profile/follow/:id', auth, async (req, res) => {
     try{
         //1. we need to find the user that we need to follow
         const userProfile = await Profile.findOne({user: req.params.id});
-        console.log(userProfile);
         if(!userProfile){
             return res.status(404).send({err: 'profile not found'});
         }
@@ -73,7 +72,7 @@ router.put('/api/profile/follow/:id', auth, async (req, res) => {
         console.log(myProfile);
         //2. push our details in that users followers
         userProfile.followers.unshift({ user: req.user._id});
-        console.log(userProfile);
+
         await userProfile.save();
         //3. push that user on our following
         myProfile.following.unshift({user: req.params.id});
@@ -126,6 +125,37 @@ router.get('/api/profile/following', auth, async (req, res) => {
     }
 });
 
+router.delete('/api/profile/unfollow/:id', auth, async (req, res) => {
+    try{
+        const userProfile = await Profile.findOne({user: req.params.id});
+        if(!userProfile){
+            return res.status(404).send({err: 'profile not found'});
+        }
+        
+        const isFollowed = userProfile.followers.find(follower => follower.user.toString() === req.user.id);
+        if(!isFollowed){
+            return res.status(400).send({err: 'You are already not following this profile'});
+        }
 
+        const myProfile = await Profile.findOne({user: req.user._id});
+        if(!myProfile){
+            return res.status(404).send({err: 'You haven\'t created a profile yet'});
+        }
+
+        const indexOfFollower = userProfile.followers.map(follower => follower.user.toString()).indexOf(req.user.id);
+        userProfile.followers.splice(indexOfFollower, 1);
+        
+        await userProfile.save();
+
+        const indexOfFollowee = myProfile.following.map( followee => followee.user.toString()).indexOf(req.params.id);
+        myProfile.following.splice(indexOfFollowee, 1);
+
+        await myProfile.save();
+
+        res.status(200).send({userProfile: userProfile.followers, myProfile: myProfile.following});
+    } catch(err){
+        res.status(500).send({ err })
+    }
+});
 
 module.exports = router;
