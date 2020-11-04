5de3a9315efcf3fb468bbc3a9378b1dbaa410272
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-11-04 02:54:08 -0800

    Changed port

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(feedRouter);
 
 
 // PORT
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=>{
     console.log('Server is up on port: ' + port);
