13e9477818f18ccb437cc0d57a238567d1391a5c
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-11-10 07:03:40 -0800

    Basic setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
+import "./App.css";
+import {
+  Switch,
+  Route,
+  BrowserRouter as Router,
+  Redirect,
+} from "react-router-dom";
+import Login from "./components/auth/Login";
+import { Provider } from "react-redux";
+import store from "./store";
+import SignUp from "./components/auth/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Initialized React App</h1>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={SignUp} />
+          <Route path="/">
+            <Redirect to="/login" />
+          </Route>
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
