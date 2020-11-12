a524f2d4039afc79ffcb698a9c87ada5fb52aa22
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-11-12 09:35:38 -0800

    Handled routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
 import "./App.css";
-import {
-  Switch,
-  Route,
-  BrowserRouter as Router,
-  Redirect,
-} from "react-router-dom";
+import { useEffect } from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Login from "./components/auth/Login";
 import { Provider } from "react-redux";
 import store from "./store";
 import SignUp from "./components/auth/Signup";
+import { loadUser } from "./actions/auth";
+import Feed from "./components/Feed";
+import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
         <Switch>
           <Route path="/login" component={Login} />
           <Route path="/register" component={SignUp} />
+          <Route path="/dashboard" component={Dashboard} />
           <Route path="/">
-            <Redirect to="/login" />
+            <Feed />
           </Route>
         </Switch>
       </Router>
