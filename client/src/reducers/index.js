cd5652a16f30908b814a6f80a51956bd0b607de4
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-11-12 09:32:57 -0800

    combined feed reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import auth from "./auth";
 import alerts from "./alert";
+import feed from "./feed";
 const rootReducer = combineReducers({
   auth,
   alerts,
+  feed,
 });
 
 export default rootReducer;
