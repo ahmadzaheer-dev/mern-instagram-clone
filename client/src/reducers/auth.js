c2ec2f618783c4fc34e87ea2b296a1a102195312
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-11-12 09:18:54 -0800

    Handled action types

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,11 @@
-import { LOGIN_SUCCESS, REGISTER_SUCCESS } from "../actions/actionTypes";
+import {
+  LOGIN_SUCCESS,
+  REGISTER_SUCCESS,
+  LOAD_SUCCESS,
+  LOAD_FAIL,
+  REGISTER_FAIL,
+  LOGIN_FAIL,
+} from "../actions/actionTypes";
 
 const initialState = {
   token: null,
@@ -9,6 +16,7 @@ const initialState = {
 const auth = (auth = initialState, action) => {
   const { type, payload } = action;
   if (type === LOGIN_SUCCESS) {
+    localStorage.setItem("token", payload.token);
     return {
       ...auth,
       token: payload.token,
@@ -16,11 +24,31 @@ const auth = (auth = initialState, action) => {
       isAuthenticated: true,
     };
   } else if (type === REGISTER_SUCCESS) {
+    localStorage.setItem("token", payload.token);
     return {
       ...auth,
       token: payload.token,
       user: payload.user,
       isAuthenticated: true,
+    };
+  } else if (
+    type === LOAD_FAIL ||
+    type === REGISTER_FAIL ||
+    type === LOGIN_FAIL
+  ) {
+    localStorage.removeItem("token");
+    return {
+      ...auth,
+      token: null,
+      isAuthenticated: false,
+      user: null,
+    };
+  } else if (type === LOAD_SUCCESS) {
+    return {
+      ...auth,
+      token: payload.token,
+      isAuthenticated: true,
+      user: payload.user,
     };
   }
   return auth;
