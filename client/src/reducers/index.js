e30deb3f58302048f3461f44a3f65818e13fc578
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-11-26 12:38:52 -0800

    Added new reducer to be combined

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 import auth from "./auth";
 import alerts from "./alert";
 import feed from "./feed";
+import profile from "./profile";
 const rootReducer = combineReducers({
   auth,
   alerts,
   feed,
+  profile,
 });
 
 export default rootReducer;
