8d4e658e8e3e6e5a9722293e85c40c987e783cf8
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-11-26 12:33:15 -0800

    Implemented dashboard component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,12 +1,70 @@
 import React from "react";
-import Navbar from "../navbar/Navbar";
+import EditProfile from "./EditProfile";
+import ChangePassword from "./ChangePassword";
+import UploadAvatar from "./UploadAvatar";
+import {
+  Route,
+  BrowserRouter as Router,
+  Link,
+  useRouteMatch,
+  Switch,
+  NavLink,
+} from "react-router-dom";
+import "../../styles/dashboard.css";
+import { connect } from "react-redux";
 
-const Dashboard = () => {
+const Dashboard = ({ isLoading }) => {
+  let { url, match } = useRouteMatch();
   return (
     <div>
-      <Navbar />
+      <div className="row flex dashboard">
+        <div className="col col-20 dashboard-link-col">
+          <NavLink
+            to={`${url}/edit`}
+            className="dashboard__link"
+            activeClassName="dashboard__link--active"
+          >
+            Edit Profile
+          </NavLink>
+          <NavLink
+            to={`${url}/password/change`}
+            className="dashboard__link"
+            activeClassName="dashboard__link--active"
+          >
+            Change Password
+          </NavLink>
+          <NavLink
+            to={`${url}/avatar`}
+            className="dashboard__link"
+            activeClassName="dashboard__link--active"
+          >
+            Upload Avatar
+          </NavLink>
+        </div>
+        <div className="col col-80 dashboard-component-col">
+          {!isLoading ? (
+            <p>Loading</p>
+          ) : (
+            <Switch>
+              <Route path={`/dashboard/edit`}>
+                <EditProfile />
+              </Route>
+              <Route path={`/dashboard/password/change`}>
+                <ChangePassword />
+              </Route>
+              <Route path={`/dashboard/avatar`}>
+                <UploadAvatar />
+              </Route>
+            </Switch>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Dashboard;
+const mapStateToProps = (state) => ({
+  isLoading: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, null)(Dashboard);
