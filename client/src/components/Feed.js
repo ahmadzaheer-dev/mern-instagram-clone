5f6eb5193a2361d7a3adb1ec6df7b931475f308d
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-11-26 12:25:52 -0800

    Implemented Feed component

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,36 +1,76 @@
-import React, { useEffect } from "react";
-import Navbar from "./navbar/Navbar";
+import React, { useEffect, Fragment } from "react";
 import Post from "./post/Post";
 import { connect } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
 import { loadFeed } from "../actions/feed";
+import { makeStyles } from "@material-ui/core/styles";
+import "../styles/feed.css";
+import { getCurrentProfile } from "../actions/profile";
+import ProfileSnippet from "./Profile/ProfileSnippet";
 
-const Feed = ({ feed, isAuthenticated, loadFeed }) => {
+const useStyles = makeStyles((theme) => ({
+  large: {
+    width: theme.spacing(10),
+    height: theme.spacing(10),
+  },
+}));
+
+const Feed = ({
+  feed,
+  isAuthenticated,
+  user,
+  loadFeed,
+  profile,
+  getCurrentProfile,
+}) => {
   useEffect(() => {
+    getCurrentProfile();
     loadFeed();
-  }, []);
-
-  if (!isAuthenticated) {
-    return <Redirect to="/login" />;
-  }
+  }, [getCurrentProfile, loadFeed]);
 
   return (
     <div>
-      <Navbar />
-      <div className="row">
-        {feed !== 0 &&
-          feed.length > 0 &&
-          feed.map((post) => {
-            return (
-              <Post
-                user={post.user}
-                imageUrl={`http://localhost:5000/api/post/image/${post.postId.image}`}
-                caption={post.postId.caption}
-                key={post._id}
-              />
-            );
-          })}
-      </div>
+      {profile.isLoading ? (
+        <p>Loading</p>
+      ) : (
+        <Fragment>
+          {profile.data ? (
+            <div className="row col-2-2-1">
+              <div className="col col-60">
+                <div className="feed-wrapper">
+                  {feed !== 0 &&
+                    feed.length > 0 &&
+                    feed.map((post) => {
+                      return (
+                        <Post
+                          user={post.user}
+                          imageUrl={`http://localhost:5000/api/post/image/${post.postId.image}`}
+                          caption={post.postId.caption}
+                          key={post._id}
+                        />
+                      );
+                    })}
+                </div>
+              </div>
+              <div className="col col-40">
+                <div className="profile-wrapper">
+                  <ProfileSnippet user={user} profile={profile} size="small" />
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div class="row">
+              <ProfileSnippet user={user} profile={profile} size="large" />
+              <h3 className="profilemsg">
+                Hi there! You don't have a profile. Let's get started.
+              </h3>
+              <Link className="btn" to="/dashboard/edit">
+                Setup Profile
+              </Link>
+            </div>
+          )}
+        </Fragment>
+      )}
     </div>
   );
 };
@@ -38,6 +78,8 @@ const Feed = ({ feed, isAuthenticated, loadFeed }) => {
 const mapStateToProps = (state) => ({
   isAuthenticated: state.auth.isAuthenticated,
   feed: state.feed,
+  user: state.auth.user,
+  profile: state.profile,
 });
 
-export default connect(mapStateToProps, { loadFeed })(Feed);
+export default connect(mapStateToProps, { loadFeed, getCurrentProfile })(Feed);
