6fc7e8dca88424ac48dd8c18094cf057fc97b22e
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-11-26 12:18:52 -0800

    allowed cross origin requests

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('./db/mongooseConn');
+const cors = require("cors");
+require("./db/mongooseConn");
 
-const userRouter = require('./routes/user');
-const profileRouter = require('./routes/profile');
-const postRouter = require('./routes/post');
-const feedRouter = require('./routes/feed');
-
+const userRouter = require("./routes/user");
+const profileRouter = require("./routes/profile");
+const postRouter = require("./routes/post");
+const feedRouter = require("./routes/feed");
 
 app.use(express.json());
+app.use(cors());
 app.use(userRouter);
 app.use(profileRouter);
 app.use(postRouter);
 app.use(feedRouter);
 
-
 // PORT
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>{
-    console.log('Server is up on port: ' + port);
-})
+app.listen(port, () => {
+  console.log("Server is up on port: " + port);
+});
