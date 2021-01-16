87f5027bcd25126a9bf0448366454907824f2181
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-01-16 02:20:29 -0800

    Added actions

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,6 +3,10 @@ import {
   PROFILE_LOADED,
   PROFILE_ERR,
   CLEAR_PROFILE,
+  POSTS_LOADED,
+  POSTS_ERR,
+  FOLLOWED_PROFILE,
+  FOLLOW_FAILED,
 } from "./actionTypes";
 import axios from "axios";
 import { setAlert } from "./alert";
@@ -66,4 +70,61 @@ export const getCurrentProfile = () => async (dispatch) => {
   }
 };
 
-export const uploadAvatar = () => (dispatch) => {};
+export const loadPosts = (username) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  try {
+    const res = await axios.get(`/api/posts/${username}`, config);
+    dispatch({
+      type: POSTS_LOADED,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: POSTS_ERR,
+    });
+  }
+};
+
+export const getProfileByUsername = (username) => async (dispatch) => {
+  dispatch({
+    type: CLEAR_PROFILE,
+  });
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  try {
+    const profile = await axios.get(`/api/profile/${username}`, config);
+    dispatch({
+      type: PROFILE_LOADED,
+      payload: profile.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERR,
+    });
+  }
+};
+
+export const followProfile = (profileId) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  try {
+    const profile = await axios.put(`/api/profile/follow/${profileId}`, config);
+    dispatch({
+      type: FOLLOWED_PROFILE,
+    });
+  } catch (err) {
+    dispatch({
+      type: FOLLOW_FAILED,
+    });
+  }
+};
