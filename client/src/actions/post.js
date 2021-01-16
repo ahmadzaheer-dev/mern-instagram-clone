6522c560e114ebe02d6df2db410b94880bb85d06
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-01-16 02:39:43 -0800

    Added actions for post creation

---
+++ client/src/actions/post.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { POST_CREATED } from "./actionTypes";
+import { setAlert } from "./alert";
+
+export const createPost = (image, caption) => async (dispatch) => {
+  const data = new FormData();
+  data.append("posts", image);
+  data.append("caption", caption);
+  try {
+    const res = await axios.post("/api/post", data);
+    dispatch({
+      type: POST_CREATED,
+      payload: res.data,
+    });
+    dispatch(setAlert("Post created successfully", "SUCCESS"));
+  } catch (err) {
+    dispatch(setAlert("Post creation failed", "DANGER"));
+  }
+};
