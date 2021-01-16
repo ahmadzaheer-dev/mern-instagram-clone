185b95983b0316ac1b03f194c6bd8fb514bd9631
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-01-16 02:38:09 -0800

    Handled posts and Follow actions

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -3,11 +3,17 @@ import {
   PROFILE_LOADED,
   PROFILE_ERR,
   CLEAR_PROFILE,
+  POSTS_LOADED,
+  POSTS_ERR,
+  FOLLOWED_PROFILE,
+  FOLLOW_FAILED,
 } from "../actions/actionTypes";
 
 const initialState = {
   data: null,
   isLoading: true,
+  posts: null,
+  isFollowed: null,
 };
 
 const profileReducer = (profile = initialState, action) => {
@@ -23,11 +29,33 @@ const profileReducer = (profile = initialState, action) => {
       ...profile,
       data: null,
       isLoading: false,
+      posts: null,
     };
   } else if (type === CLEAR_PROFILE) {
     return {
       data: null,
       isLoading: true,
+      posts: null,
+    };
+  } else if (type === POSTS_LOADED) {
+    return {
+      ...profile,
+      posts: payload,
+    };
+  } else if (type === POSTS_ERR) {
+    return {
+      ...profile,
+      posts: null,
+    };
+  } else if (type === FOLLOWED_PROFILE) {
+    return {
+      ...profile,
+      isFollowed: true,
+    };
+  } else if (type === FOLLOW_FAILED) {
+    return {
+      ...profile,
+      isFollowed: false,
     };
   }
 
