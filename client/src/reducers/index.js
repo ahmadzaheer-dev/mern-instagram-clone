984376238116a4cf7f963bde266a602c983e20de
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-01-16 02:36:00 -0800

    Combined reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,11 +3,15 @@ import auth from "./auth";
 import alerts from "./alert";
 import feed from "./feed";
 import profile from "./profile";
+import post from "./post";
+import search from "./search";
 const rootReducer = combineReducers({
   auth,
   alerts,
   feed,
   profile,
+  post,
+  search,
 });
 
 export default rootReducer;
