34f0c86d16571f2308b0a279e2cde6673261c648
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-01-24 10:01:54 -0800

    Added views for all followers and following

--- client/src/components/Profile/ProfileData.js
+++ client/src/components/Profile/ProfileData.js
@@ -1,4 +1,4 @@
-import { React } from "react";
+import { React, useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import { makeStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
@@ -6,6 +6,8 @@ import "../../styles/profile.css";
 import { Link } from "react-router-dom";
 import Loading from "../loading/Loading";
 import { followProfile } from "../../actions/profile";
+import Modal from "react-modal";
+import CloseIcon from "@material-ui/icons/Close";
 
 const useStyles = makeStyles((theme) => ({
   xlarge: {
@@ -20,6 +22,23 @@ const ProfileData = ({ profile, user, posts, isLoading, followProfile }) => {
   const onFollowClick = (e) => {
     followProfile(profile.user._id);
   };
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [isFollowingModal, setIsFollowingModal] = useState(false);
+
+  const openModal = async (isFollowing) => {
+    if (isFollowing) {
+      await setIsFollowingModal(true);
+    } else {
+      await setIsFollowingModal(false);
+    }
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div>
       <div className="profile-data grid-2-1-2">
@@ -64,13 +83,23 @@ const ProfileData = ({ profile, user, posts, isLoading, followProfile }) => {
               <span className="profile-data__stats-count">
                 {profile.followers.length}
               </span>{" "}
-              followers
+              <button
+                className="profile-data__btn--transparent"
+                onClick={(e) => openModal(false)}
+              >
+                followers
+              </button>
             </p>
             <p className="profile-data__stats">
               <span className="profile-data__stats-count">
                 {profile.following.length}
               </span>{" "}
-              following
+              <button
+                className="profile-data__btn--transparent"
+                onClick={(e) => openModal(true)}
+              >
+                following
+              </button>
             </p>
           </div>
           <h3 className="profile-data__fullname">{profile.name}</h3>
@@ -79,6 +108,71 @@ const ProfileData = ({ profile, user, posts, isLoading, followProfile }) => {
             {profile.website}
           </a>
         </div>
+        <Modal
+          className="follow-modal"
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          contentLabel="Example Modal"
+          style={{
+            overlay: {
+              position: "fixed",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              backgroundColor: "rgba(0, 0, 0, 0.70)",
+            },
+          }}
+        >
+          <div className="follow-modal__topbar">
+            {isFollowingModal ? (
+              <h3 className="follow-modal__heading">Follwing</h3>
+            ) : (
+              <h3 className="follow-modal__heading">Followers</h3>
+            )}
+
+            <button className="follow-modal__close" onClick={closeModal}>
+              <CloseIcon />
+            </button>
+          </div>
+          <div className="follow-modal__users">
+            {isFollowingModal
+              ? profile.following.map((user) => {
+                  return (
+                    <div className="search-result__user">
+                      <Avatar
+                        alt={user.user.username.toUpperCase()}
+                        src={`http://localhost:5000/api/user/avatar/${user.user.avatar}`}
+                        variant="circle"
+                      />
+                      <Link
+                        className="search-result__username"
+                        to={`/${user.user.username}`}
+                      >
+                        {user.user.username}
+                      </Link>
+                    </div>
+                  );
+                })
+              : profile.followers.map((user) => {
+                  return (
+                    <div className="search-result__user">
+                      <Avatar
+                        alt={user.user.username.toUpperCase()}
+                        src={`http://localhost:5000/api/user/avatar/${user.user.avatar}`}
+                        variant="circle"
+                      />
+                      <Link
+                        className="search-result__username"
+                        to={`/${user.user.username}`}
+                      >
+                        {user.user.username}
+                      </Link>
+                    </div>
+                  );
+                })}
+          </div>
+        </Modal>
       </div>
     </div>
   );
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { Avatar } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import { ChatBubbleOutline, FavoriteBorder } from "@material-ui/icons";
